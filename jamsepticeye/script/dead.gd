482496e1124bee366394d76197eae1843ba04736
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-10-05 20:09:00 -0500

    push

--- jamsepticeye/script/dead.gd
+++ jamsepticeye/script/dead.gd
@@ -6,6 +6,12 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func _on_button_pressed() -> void:
+	print("wow")
+	get_tree().change_scene_to_file("res://scenes/3d/3d_scene_1.tscn") 
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
